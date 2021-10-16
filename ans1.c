035636fb336c96bb2603208e1a6e21910a479eb9
Palak1510 <ptadhani@lakheadu.ca>
2021-10-16 17:34:15 -0400

    Assignment-1

---
+++ ans1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    //declaring variable types
+    int d = 10;
+    char p = 'a';
+    float q = 8.0;
+    double s = 9.00;
+    long r = 123456789.0;
+
+    // printing primitive datatpes
+    printf("%d\n", d);
+    printf("%c\n", p);
+    printf("%f\n", q);
+    printf("%l\n", d);
+    printf("%ld\n", r);
+    return 0;
+}
+
